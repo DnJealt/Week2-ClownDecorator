c681fb7588950d578a0077f2dcdfdea6fb74187f
Rick van Osta <osta1995@hotmail.com>
2014-11-19 12:40:05 +0100

    canvaspanel

--- Clowndinges/Clowndinges/MainWindow.xaml.cs
+++ Clowndinges/Clowndinges/MainWindow.xaml.cs
@@ -22,6 +22,30 @@ namespace Clowndinges
         public MainWindow()
         {
             InitializeComponent();
+            CreateDynamicCanvasPanel();
+        }
+
+        private void CreateDynamicCanvasPanel()
+        {
+            // Create a Canvas Panel control
+            Canvas canvasPanel = new Canvas();
+            // Set Canvas Panel properties
+            canvasPanel.Background = new SolidColorBrush(Colors.LightCyan);
+
+            Ellipse hoofd1 = new Ellipse();
+            hoofd1.Width = 140;
+            hoofd1.Height = 140;
+            hoofd1.Stroke = new SolidColorBrush(Colors.Black);
+            hoofd1.Fill = new SolidColorBrush(Colors.White);
+            Canvas.SetLeft(hoofd1, 60);
+            Canvas.SetTop(hoofd1, 60);
+            canvasPanel.Children.Add(hoofd1);
+
+           
+
+
+            // Set Grid Panel as content of the Window
+            this.Content = canvasPanel;
         }
     }
 }
